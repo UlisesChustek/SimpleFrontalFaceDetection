63bb3e8f815765181dc4913acecd1f3b7e594ddc
Ulises Chustek <uliseschustek@gmail.com>
2022-11-23 15:08:57 -0300

    Given some comentary intructions on main.py to change the functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 
 clf = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)#If you want to put more cameras you need to change this value, by default it's 0 means that works with 1 camera#
 
 while True:
     _, frame= camera.read()
@@ -20,7 +20,7 @@ while True:
         cv2.rectangle(frame,(x,y),(x+width, y+height),(255,255,0),2)
         
     cv2.imshow("Faces",frame)
-    if cv2.waitKey(1) == ord("q"):
+    if cv2.waitKey(1) == ord("q"):#If you want to change the key to stop the program you need to change this value#
         break
 camera.release()
 cv2.destroyAllWindows()
